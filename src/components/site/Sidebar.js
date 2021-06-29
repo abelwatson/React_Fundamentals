c68783a182b5631e3bb814cc1c1040f5f2df6f3d
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-06-28 20:20:39 -0400

    completed through 7

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -5,7 +5,12 @@ import {
 } from 'react-router-dom';
 import Home from './Home';
 import Resources from './Resources';
-import FunctionalComponentDemo from '../concepts/FunctionalComponents/FunctionalComponentsDemo';
+import FunctionalComponentDemo from '../concepts/FunctionalComponentsDemo';
+import JSXRules from '../concepts/JSXRules';
+import State from '../concepts/State';
+import Effects from '../concepts/Effects';
+import PropsDemo from '../concepts/PropDemo';
+import Hooks from '../concepts/Hooks';
 
 const Sidebar = () => {
     return (
@@ -15,6 +20,11 @@ const Sidebar = () => {
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/resources'>Resources</Link></li>
                     <li><Link to='/functionalcomponent'>Functional Component</Link></li>
+                    <li><Link to='/jsxrules'>JSX Rules</Link></li>
+                    <li><Link to='/state'>State</Link></li>
+                    <li><Link to='/effects'>Effects</Link></li>
+                    <li><Link to='/propsdemo'>PropsDemo</Link></li>
+                    <li><Link to='/hooks'>Hooks</Link></li>
                 </ul>
             </div>
             <div className='sidebar-route'>
@@ -23,6 +33,11 @@ const Sidebar = () => {
                     <Route exact path='/resources'><Resources /></Route>
                     <Route exact path='/'><Home /></Route>
                     <Route exact path='/functionalcomponent'><FunctionalComponentDemo /></Route>
+                    <Route exact path='/jsxrules'><JSXRules /></Route>
+                    <Route exact path='/state'><State /></Route>
+                    <Route exact path='/effects'><Effects /></Route>
+                    <Route exact path='/propsdemo'><PropsDemo /></Route>
+                    <Route exact path='/hooks'><Hooks /></Route>
                 </Switch>
             </div>
         </div>
